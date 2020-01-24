1762aedc29a3b4e1da5b05a1575ab2e269ffe5cd
joserobjr <jose.rob.jr@gmail.com>
2020-01-23 22:25:55 -0300

    1.15.2

--- src/main/java/net/fabricmc/example/ExampleMod.java
+++ src/main/java/net/fabricmc/example/ExampleMod.java
@@ -15,7 +15,7 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class ExampleMod implements ModInitializer {
-	@Override
+	//@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
